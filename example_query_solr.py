cd395fe6608520fd0f7e41ddc66328ec611fd93c
Iuliia Pavlova <44636761+Juls777@users.noreply.github.com>
2018-11-20 20:42:50 +0200

    Update example_query_solr.py

--- example_query_solr.py
+++ example_query_solr.py
@@ -1,8 +1,12 @@
+# Making a query is a simple matter. First, tell Python you will need to make HTTP connections.
 from urllib.request import *
 
+# Now open a connection to the server and get a response. The wt query parameter tells Solr to return 
+# results in a format that Python can understand.
 connection = urlopen('http://localhost:8983/solr/wiki/select?q=text:cheese&wt=python&start=0&rows=100')
 response = eval(connection.read())
 
+# Now interpreting the response is just a matter of pulling out the information that you need.
 print(response['response']['numFound'], "documents found.")
 
 # Print the name of each document.
