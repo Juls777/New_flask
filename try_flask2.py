8c8f30672a953c533fa388a24f4d925f92f82ef4
Iuliia Pavlova <44636761+Juls777@users.noreply.github.com>
2018-11-21 12:56:34 +0200

    Update try_flask2.py

--- try_flask2.py
+++ try_flask2.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 20 23:17:31 2018
-
-@author: Tom
-"""
-
-from urllib.request import *
 from flask import Flask, render_template, request, redirect, url_for
+from urllib.request import *
 
 #Initialize Flask instance
 app = Flask(__name__)
@@ -16,20 +9,8 @@ example_data = [
     {"name": "Misty forest", "source": "forest.jpg"},
     {"name": "Bonfire burning", "source": "fire.jpg"},
     {"name": "Old library", "source": "library.jpg"},
-    {"name": "Sliced orange", "source": "orange.jpg"},
-    {"name": "query", "source": "http://localhost:8983/solr/wiki/select?q=text:title&wt=python&start=0&rows=10"}
+    {"name": "Sliced orange", "source": "orange.jpg"}
 ]
-response = eval(example_data.read())
-
-print(response['response']['numFound'], "documents found.")
-
-# Print the name of each document.
-
-print("Printing 10 top documents:")
-
-for i, document in enumerate(response['response']['docs']):
-      print("  Document title ", i, "=", document['title'])
-
 
 #Use "query" variable from the URL. If no variable is given,
 #use empty string instead. GET and POST methods are allowed.
@@ -47,6 +28,7 @@ def search(query):
 
     matches = []
 
+
     #If an entry name contains the query, add the entry to matches.
     if query != "":
         for entry in example_data:
@@ -55,3 +37,38 @@ def search(query):
  
     #Render index.html with matches variable. 
     return render_template("index.html", matches=matches)
+
+@app.route("/solr", defaults={"query": ""}, methods=["GET", "POST"])
+@app.route("/solr/<query>", methods=["GET", "POST"])
+def solr(query):
+
+    if request.method == "POST":
+        #Get query from the POST form.
+        query = request.form["query"]
+        
+        #Redirect to the same page with the query in the url.
+        #ALWAYS REDIRECT AFTER POSTING!
+        return redirect(url_for("solr", query=query))
+
+    matches = []
+
+# Now open a connection to the server and get a response. The wt query parameter tells Solr to return 
+# results in a format that Python can understand.
+connection = urlopen('http://localhost:8983/solr/wiki/select?q=text:' + query + '&wt=python&start=0&rows=100&fi=title,id')
+response = eval(connection.read())
+
+# Now interpreting the response is just a matter of pulling out the information that you need.
+print(response['response']['numFound'], "documents found.")
+
+# Print the name of each document.
+
+print("Printing 100 top documents:")
+
+for i, document in enumerate(response['response']['docs']):
+      #print("  Document title ", i, "=", document['title'])
+        match = {"title":document['title'], "text": document['id']}
+        matches.append(match)
+   
+#Render index.html with matches variable.
+return render_template("solr.html", matches=matches)
+    
